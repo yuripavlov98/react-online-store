16ad169885511c48a437486fba09695aa8ffa34c
Yuri Pavlov <yuripavlovdex@gmail.com>
2023-03-24 14:12:23 +0300

    described models

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 require('dotenv').config()
 const express = require('express')
+const sequelize = require('./db')
+const models = require('./models/models')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 
-app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
+// запускаю сервер
+const start = async () => {
+    try {
+        await sequelize.authenticate() // аутентификация
+        await sequelize.sync() // сверяет бд со схемой данных
+        app.listen(PORT, () => console.log(`Server start on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
