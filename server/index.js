790027ac28bc8cf2bf1efa17dd3074b872faea32
Yuri Pavlov <yuripavlovdex@gmail.com>
2023-03-24 16:38:14 +0300

    added filtering, pagination, getting products

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
+app.use(express.static(path.resolve(__dirname, 'static'))) // сохранение картинок в 'указанную папку'
 app.use(fileUpload({})) // картинки
 app.use('/api', router)
 
