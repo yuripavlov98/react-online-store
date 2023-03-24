ed3f6cbaa0425bcec3e7e33907927826714253fd
Yuri Pavlov <yuripavlovdex@gmail.com>
2023-03-24 15:57:23 +0300

    added crud for product

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,5 +1,21 @@
+const uuid = require('uuid') // генерация id
+const path = require('path') // путь к изображению
+const {Device} = require('../models/models')
+const ApiError = require('../error/ApiError')
 class DeviceController {
-    async create(req, res) {
+    async create(req, res, next) {
+        try {
+            const {name, price, brandId, typeId, info} = req.body 
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg" // генерация id
+            img.mv(path.resolve(__dirname, '..', 'static', fileName)) //путь до текущей папки, вернуться на директорию выше, папка
+    
+            const device = await Device.create({name, price, brandId, typeId, img: fileName})
+    
+            return res.json(device)
+        } catch (e) {
+            next(ApiError.badRequest(e.message))
+        }
 
     }
     async getAll(req, res) {
--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -8,7 +8,8 @@ class TypeController {
         return res.json(type)
     }
     async getAll(req, res) {
-        
+        const types = await Type.findAll()
+        return res.json(types)
     }
 }
 
