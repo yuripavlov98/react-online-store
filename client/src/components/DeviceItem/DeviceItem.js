c26dc71ebb9499b885a92b48f01231dc3da46000
Yuri Pavlov <yuripavlovdex@gmail.com>
2023-03-30 13:36:45 +0300

    styled the product page

--- client/src/components/DeviceItem/DeviceItem.js
+++ client/src/components/DeviceItem/DeviceItem.js
@@ -3,10 +3,14 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image'
 import star from '../../assets/star.png'
+import { useNavigate } from "react-router-dom"
+import { DEVICE_ROUTE } from '../../utils/consts';
 
 const DeviceItem = ({device}) => {
+    const navigate = useNavigate()
+
     return (
-        <Col md={3} className='mt-3'>
+        <Col md={3} className='mt-3' onClick={() => navigate(DEVICE_ROUTE + '/' + device.id)}>
             <Card style={{width: 200, cursor: 'pointer'}} border={'light'}>
                 <Image width={200} height={200} src={device.img}/>
                 <div className='mt-1'>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import star_card from '../assets/star_card.png'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const DevicePage = () => {
+    const device = {id: 1, name: "Cмартфон Apple iPhone 12 pro", price: 49990, rating: 5, img: "9a56ba70-de47-47d9-a545-ba78e1206999.jpg"}
+    const description = [
+        {id:1, title: 'Гарантия', description: '1 год'},
+        {id:4, title: 'Модель', description: 'MGMK3RU/A'},
+        {id:6, title: 'Разрешение экрана', description: '2532x1170 Пикс'},
+        {id:7, title: 'Экран', description: '6.1"/2532x1170 Пикс'},
+        {id:8, title: 'Технология экрана', description: 'OLED'},
+        {id:9, title: 'Безрамочный', description: 'Да'},
+        {id:10, title: 'Тип экрана', description: 'Super Retina XDR'},
+        {id:11, title: 'Частота обновления', description: '60 Гц'},
+        {id:13, title: 'Тип процессора', description: 'A14 Bionic'},
+        {id:14, title: 'Встроенная память (ROM)', description: '128 ГБ'},
+    ]
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className='mt-4'>
+            <Row>
+                <Col md={4}>
+                    <Image width={400} height={500} src={device.img}/>
+                </Col>
+                <Col md={6}>
+                    <div className='d-flex align-items-center'>
+                        <h1 style={{fontSize: 26}}>{device.name}</h1>
+                        <div className='d-flex align-items-center justify-content-center'
+                        style={{background: `url(${star_card}) no-repeat center center`, 
+                        width: 24, height: 23, 
+                        backgroundSize: 'cover', 
+                        fontSize: 14}}>
+                            {device.rating}
+                        </div>
+                    </div>
+                    <Row className='d-flex flex-column mt-3'>
+                    <h2 style={{fontSize: 20}}>Характеристики</h2>
+                        <ul style={{width: 630}}>
+                            {description.map(info => 
+                                <li key={info.id} 
+                                className='d-flex justify-content-between pb-1 mb-2 ms-1' 
+                                style={{borderBottom: '1px dashed #bfbdb2'}}>
+                                    <span>{info.title}</span>
+                                    <span>{info.description}</span>
+                                </li>
+                            )}
+                        </ul>
+                    </Row>
+                </Col>
+                <Col md={2}>
+                    <Card className='d-flex flex-column align-items-center justify-content-around'
+                    style={{width: 300, height: 200, fontSize: 32,  backgroundColor: '#f5f5f6'}}>
+                        <span>{device.price} ₽</span>
+                        <Button variant='danger'>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
